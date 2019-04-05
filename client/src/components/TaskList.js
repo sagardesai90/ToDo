2c114f9d74faa810db615cd36f29f09a6ab3ce50
sagardesai90 <saagardesai@gmail.com>
2019-04-05 17:24:20 -0500

    connected redux to backend

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -21,14 +21,14 @@ class TaskList extends Component {
                 
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
-                        {tasks.map(({ id, name }) => (
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {tasks.map(({ _id, name }) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
                                 <Button
                                     className="remove-btn"
                                     color="danger"
                                     size="sm"
-                                    onClick={this.onDeleteClick.bind(this, id)}
+                                    onClick={this.onDeleteClick.bind(this, _id)}
                                 > &times;
                                 </Button>
                                     {name}
--- client/src/reducers/taskReducer.js
+++ client/src/reducers/taskReducer.js
@@ -2,24 +2,23 @@ import uuid from 'uuid';
 import {
   GET_TASKS,
   ADD_TASKS,
-  DELETE_TASK
+  DELETE_TASK,
+  TASKS_LOADING
 } from '../actions/constants';
 
 const initialState = {
   
-  tasks: [
-    { id: uuid(), name: 'Eggs'},
-    { id: uuid(), name: 'Milk'},
-    { id: uuid(), name: 'Juice'},
-    { id: uuid(), name: 'Orange'}
-  ]
+  tasks: [],
+  loading: false
 }
 
 export default function(state = initialState, action){
   switch(action.type) {
     case GET_TASKS:
       return {
-        ...state
+        ...state,
+        tasks: action.payload,
+        loading: false
       }
     case ADD_TASKS:
       return {
@@ -29,8 +28,13 @@ export default function(state = initialState, action){
     case DELETE_TASK:
       return {
         ...state,
-        tasks: state.tasks.filter(task => task.id !== action.payload)
-      }
+        tasks: state.tasks.filter(task => task._id !== action.payload)
+      };
+    case TASKS_LOADING:
+      return {
+        ...state,
+        loading: true
+      }; 
     default:
       return state;
   }
