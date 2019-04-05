3cdb60a72b743e101b954a1969593e569439e2b0
sagardesai90 <saagardesai@gmail.com>
2019-04-04 21:59:33 -0500

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import AppNav from './components/AppNav';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello</h1>
+        <AppNav />
       </div>
     );
   }
